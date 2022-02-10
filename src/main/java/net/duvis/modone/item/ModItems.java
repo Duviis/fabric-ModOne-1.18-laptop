eddafc12b7bc2b54455d7c88a535ed5a31f8828a
Duviis <duvi.huvi@gmail.com>
2022-02-10 14:48:43 +0100

    Added the DowsingRod

--- src/main/java/net/duvis/modone/item/ModItems.java
+++ src/main/java/net/duvis/modone/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.duvis.modone.item;
 
 import net.duvis.modone.ModOne;
+import net.duvis.modone.item.custom.DowsingRodItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -17,6 +18,9 @@ public class ModItems {
 
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxDamage(64)));
 
     private static Item registerItem(String name,Item item) {
         return Registry.register(Registry.ITEM, new Identifier(ModOne.MOD_ID, name), item);
